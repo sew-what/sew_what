ab4be901cf3c6cda1bbe4573a482e786764ab6fa
Orion Musselman <orionmm@pm.me>
2022-02-20 19:56:51 -0500

    Now that's THICC

--- test.cpp
+++ test.cpp
@@ -18,7 +18,7 @@ struct map {
  * x = x coordinate of circle
  * y = y coordinate of circle
  * */
-void drawCircle(map &m, int diameter, int x, int y) {
+void drawCircle(map &m, int diameter, int x, int y, int weight) {
     int radius = diameter / 2;
     int lx = x - radius;
 
@@ -26,21 +26,17 @@ void drawCircle(map &m, int diameter, int x, int y) {
     for (int i = 0; i < diameter; i++) {
         int xi = lx + i;
         int ty = std::round(std::sqrt(std::pow(radius, 2) - std::pow(i - radius, 2)));
-        m.map[xi + (y + ty) * m.width] = 0xFF;
-        m.map[xi + (y - ty) * m.width] = 0xFF;
-        m.map[(x + ty) + xi * m.width] = 0xFF;
-        m.map[(x - ty) + xi * m.width] = 0xFF;
-    }
-}
+        for (int w = 0; w < weight; w++) {
+            m.map[xi + (y + ty + w) * m.width] = 0xFF;
+            m.map[xi + (y - ty - w) * m.width] = 0xFF;
+            m.map[(x + ty + w) + xi * m.width] = 0xFF;
+            m.map[(x - ty - w) + xi * m.width] = 0xFF;
 
-void drawCircleWeight(map &m, int diameter, int x, int y, int weight) {
-    drawCircle(m, diameter, x, y);
-
-    weight--;
-
-    for (int i = 1; i <= weight; i++) {
-        drawCircle(m, diameter - i * 2, x, y);
-        drawCircle(m, diameter + i * 2, x, y);
+            m.map[xi + (y + ty - w) * m.width] = 0xFF;
+            m.map[xi + (y - ty + w) * m.width] = 0xFF;
+            m.map[(x + ty - w) + xi * m.width] = 0xFF;
+            m.map[(x - ty + w) + xi * m.width] = 0xFF;
+        }
     }
 }
 
@@ -58,8 +54,8 @@ map drawHoopSkirt(int waist, int height, int thickness = 1) {
     // drawCircle(m, waist, w / 2 - waist / 2, w / 2, w / 2);
     // drawCircle(m, waist + height * 2, 0, w / 2, w / 2);
 
-    drawCircleWeight(m, waist, w / 2, w / 2, 2);
-    drawCircleWeight(m, waist + height * 2 - 2 * 2, w / 2, w / 2, 2);
+    drawCircle(m, waist, w / 2, w / 2, 2);
+    drawCircle(m, waist + height * 2 - 2 * 2, w / 2, w / 2, 2);
 
     return m;
 }
@@ -68,10 +64,6 @@ map drawHoopSkirt(int waist, int height, int thickness = 1) {
 int main() {
     map m = drawHoopSkirt(100, 300, 1);
 
-    m.map[0] = 100;
-    m.map[0] = 100;
-    m.map[2] = 100;
-    m.map[3] = 100;
     stbi_write_bmp("test.bmp", m.width, m.height, 1, (const void *)m.map.data());
 
     return 0;
